d8b8b7685db877973839d9c69a1d5beb1187a189
Timothy Choi <TurokChoi@gmail.com>
2018-10-19 18:14:23 -0700

    more setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
+import App from 'app';
+
+
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
