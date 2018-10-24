055fbe244c3f1fe32d85636935aebf2da68b0a66
Timothy Choi <TurokChoi@gmail.com>
2018-10-24 15:56:39 -0700

    basic layout

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Contact extends Component{
+  render(){
+    return(
+      <div>
+        <div> Joey Albano : </div>
+        <div> Timothy Choi: turokchoi@gmail.com </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Link, BrowserRouter, Route, Switch} from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import App from './App';
 
-
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+import Header from './components/header';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -18,8 +21,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
+        <Header/>
         <Switch>
-          <Route path ="/" component={App}/>
+          <Route path = "/contact" component={Contact} />
+          <Route path = "/about" component={About} />
+          <Route path = "/" component={Home}/>
         </Switch>
       </div>
     </BrowserRouter>
