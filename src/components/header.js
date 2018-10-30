d8d0fb6d7d29c522dadbd359694acd65051f7f74
Timothy Choi <TurokChoi@gmail.com>
2018-10-29 22:53:26 -0700

    some style

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import '../style/header.css';
 
 export default class Header extends Component{
   render(){
     return(
-      <div>
-        <Link to = "/" > Home </Link>
-        <Link to = "/about" > About </Link>
-        <Link to = "/contact" > Contact </Link>
+      <div class = "container">
+      <div> Melee Machine Learning </div>
+        <div class = "row">
+          <div class = "col-lg-3 col-lg-offset-1 text-center">
+            <Link to = "/" > Home </Link>
+            <Link to = "/about" > About </Link>
+            <Link to = "/contact" > Contact </Link>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {Link, BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
-import App from './App';
 
 import Home from './components/home';
 import About from './components/about';
